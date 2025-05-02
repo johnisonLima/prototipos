ec9b1c3105c53831070c168dbe58481f875aca11
Johnison Almeida Lima <123204896+johnisonLima@users.noreply.github.com>
2025-05-01 23:05:17 -0300

    Implementar a estrutura inicial do projeto com Spring Boot, configuração do MySQL e configuração do Docker

---
+++ sistema-nova/nova_back/src/main/java/com/novaesquadria/nova_back/NovaBackApplication.java
@@ -0,0 +1,12 @@
+package com.novaesquadria.nova_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaBackApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NovaBackApplication.class, args);
+	}
+
+}
---
+++ sistema-nova/nova_back/src/test/java/com/novaesquadria/nova_back/NovaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novaesquadria.nova_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
